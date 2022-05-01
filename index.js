5326a32ef49230da655f5e5e171157d36095514a
cmwschroeder <cmwschroeder@gmail.com>
2022-05-01 10:50:13 -0700

    outlined functions needed to be written, added question asked to add a department

--- index.js
+++ index.js
@@ -2,13 +2,61 @@ const inquirer = require('inquirer');
 const mysql2 = require('mysql2');
 
 const menuQuestion = [
-{
-    type: 'list',
-    name: 'next',
-    message: 'Would you like to: ',
-    choices: ['View All Employees', 'Add Employees', 'Update Employee Role', 'View all Roles', 'Add Role', 'View all Departments', 'Add Department'],
-}
+    {
+        type: 'list',
+        name: 'next',
+        message: 'Would you like to: ',
+        choices: ['View All Employees', 'Add Employees', 'Update Employee Role', 'View all Roles', 'Add Role', 'View all Departments', 'Add Department', 'Quit'],
+    }
 ];
+
+const addDepartmentQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter the name of the department: ",
+    },
+];
+
+//This function shows all the employees in the db
+function viewAllEmployees() {
+
+};
+
+//This function shows all the roles in the db
+function viewAllRoles() {
+
+};
+
+//This function shows all the departments in the db
+function viewAllDepartments() {
+
+};
+
+//Adds an employee to the database
+function addEmployee() {
+
+};
+
+//Adds an role to the database
+function addRole() {
+
+};
+
+//Adds an department to the database
+function addDepartment() {
+
+}
+
+//Updates the employee info in the database
+function updateEmployee() {
+
+}
+
+//Updates the role info in the database
+function updateRole() {
+
+}
 
 //this function handles the menu for selecting what we want to do
 function menu() {
@@ -28,6 +76,8 @@ function menu() {
             case 'View all Departments':
                 break;
             case 'Add Department':
+                break;
+            default:
                 break;
         }
     })
